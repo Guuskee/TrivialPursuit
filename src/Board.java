c3e067ee6cf6d4bb6d64c646f053aac491f4e809
GBecking <trollgod92>
2018-08-09 12:35:39 +0200

    Committee

--- src/Board.java
+++ src/Board.java
@@ -1,9 +1,5 @@
 import java.awt.*;
 
 public class Board {
-    private void draw(Graphics g) {
-        Graphics2D g2d = (Graphics2D) g;
-        
 
-    }
 }
--- src/Player.java
+++ src/Player.java
@@ -1,2 +1,8 @@
 public class Player {
+    int pos; // array position integer
+    String name; // player name
+    int id; // player ID
+    boolean isTurn; // True = players turn, False = player waits
+    int amtWedges; // amount of wedges
+    boolean haveRed, haveYellow, haveGreen, havePurple, havePink, haveBlue;
 }
